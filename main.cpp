d7363b689384d429003d66c77b2ace73f500076a
VaibhavBhard <108005795+VaibhavBhard@users.noreply.github.com>
2022-08-27 07:40:09 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -142,15 +142,9 @@ system("CLS");
 //cpo
 int login(){
    string pass ="";
-   char ch;
    cout <<"\n\n\n\n\n\n\n\t\t\t\t\t  Simple Car Rental System Login";
    cout << "\n\n\n\n\n\n\n\t\t\t\t\t\tEnter Password: ";
-   ch = _getch();
-   while(ch != 13){//character 13 is enter
-      pass.push_back(ch);
-      cout << '*';
-      ch = _getch();
-   }
+  cin>>pass; 
    if(pass == "pass"){
       cout << "\n\n\n\n\t\t\t\t\t  Access Granted! Welcome To Our System \n\n";
       system("PAUSE");
